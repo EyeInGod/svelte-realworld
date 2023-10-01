18cbb9db3126a6e44ca03938f459b0e2ff2abcd0
denis henry <denysmartin4952@gmail.com>
2023-09-30 23:21:08 -0700

    build/post article, edit article page

--- src/routes/article/[slug]/+page.server.js
+++ src/routes/article/[slug]/+page.server.js
@@ -1,4 +1,5 @@
 import * as api from '$lib/api.js';
+import { error, redirect } from '@sveltejs/kit';
 
 export async function load({ locals, params }) {
     const [{ article }, { comments }] = await Promise.all([
@@ -10,5 +11,26 @@ export async function load({ locals, params }) {
 }
 
 export const actions = {
-    
+    createComment: async ({ locals, params, request }) => {
+		if (!locals.user) throw error(401);
+
+		const data = await request.formData();
+
+		await api.post(
+			`articles/${params.slug}/comments`,
+			{
+				comment: {
+					body: data.get('comment')
+				}
+			},
+			locals.user.token
+		);
+	},
+    deleteArticle: async ({ locals, params }) => {
+		if (!locals.user) throw error(401);
+
+		await api.del(`articles/${params.slug}`, locals.user.token);
+		throw redirect(307, '/');
+	},
+
 }
---
+++ src/routes/editor/[slug]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Editor from './../Editor.svelte';
+
+	export let data;
+	export let form;
+</script>
+
+<Editor article={data.article} errors={form?.errors} />
